39906449e3569c946587a30ca74a9116dec4319f
craigojo <110014616+craigojo@users.noreply.github.com>
2023-07-27 23:50:07 +1000

    Week 5 assignment

---
+++ UNE/COSC01/Python practice/Assignment_week5.py
@@ -0,0 +1,24 @@
+# set d = (key, value)
+d = {'pencil': 10, 'pen': 20, 'paper': 4, 'eraser': 80}
+
+total = 0
+
+for i in d:
+    total += d[i]
+
+ item_total = total
+
+
+d = {'pencil': 10, 'pen': 20, 'paper': 4, 'eraser': 80}
+
+total = 0
+
+for i in d:
+    total += d[i]
+
+ item_total = total
+
+    
+    
+    
+print(sum(d.values()))
