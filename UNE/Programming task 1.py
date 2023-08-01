27fd12af29f436c4acc2a6d5ea65aa018404cb34
craigojo <110014616+craigojo@users.noreply.github.com>
2023-08-01 21:00:26 +1000

    rename file

---
+++ UNE/Programming task 1.py
@@ -0,0 +1,40 @@
+'''Please enter the route number: 123
+Please enter the number of stops on this route: 4
+How many passengers were waiting for the bus at stop #1? 5
+How many passengers left the bus at stop #2? 5
+How many passengers were waiting for the bus at stop #2? 5
+How many passengers left the bus at stop #3? 5
+How many passengers were waiting for the bus at stop #3? 5
+How many passengers left the bus at stop #4? 5
+Route number: 123
+Happy customers: 15
+Unhappy customers: 0
+Ratio of happy to unhappy customers: 0.00'''
+
+input_a = input("Please enter the number of stops on this route: " )
+route_number = int(input_a)
+
+max_passengers = 35
+
+
+
+
+n = int(input('How many stops does this route have?'))       #n is the number of items you want to enter
+dict ={}                     
+for i in range(n):        
+    text = input().split()     #split the input text based on space & store in the list 'text'
+    dict[text[0]] = text[1]       #assign the 1st item to key and 2nd item to value of the dictionary
+print(dict)
+
+
+
+
+x = how many stops will there be 
+stop = (x)
+dict{}
+while i < x:
+     
+    dict['Stop' [stop] ]"hot many passengers at stop  :" [stop] )
+    stop += stop
+
+
