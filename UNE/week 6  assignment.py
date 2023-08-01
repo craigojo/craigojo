d58c95123f8adf94a83cc3a188c678a32207be75
craigojo <110014616+craigojo@users.noreply.github.com>
2023-08-01 20:58:54 +1000

    Fix syntax

--- UNE/week 6  assignment.py
+++ UNE/week 6  assignment.py
@@ -1,9 +1,11 @@
-result = 1
-n=1
-while n != 0:
-  n = n - 1
-  result = result * n
-  return result
+def factorial(n):
+  result = 1
+  n=1
+
+  while n != 0:
+    n = n - 1
+    result = result * n
+    return result
 
 for i in range(-1, 5):
   print('Factorial of', i, 'is', factorial(i))
