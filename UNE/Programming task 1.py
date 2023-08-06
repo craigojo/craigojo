4369a2b9ca6759cd0c27cd2fb437090bb014b5c8
craigojo <110014616+craigojo@users.noreply.github.com>
2023-08-06 17:39:09 +1000

    updates

--- UNE/Programming task 1.py
+++ UNE/Programming task 1.py
@@ -1,59 +1,84 @@
-'''Please enter the route number: 123
-Please enter the number of stops on this route: 4
-How many passengers were waiting for the bus at stop #1? 5
-How many passengers left the bus at stop #2? 5
-How many passengers were waiting for the bus at stop #2? 5
-How many passengers left the bus at stop #3? 5
-How many passengers were waiting for the bus at stop #3? 5
-How many passengers left the bus at stop #4? 5
-Route number: 123
-Happy customers: 15
-Unhappy customers: 0
-Ratio of happy to unhappy customers: 0.00'''
+
 
 max_passengers = 35
 
 unhappy = 0
-total_passanger=0
+total_passengers=0
 
 current_load = 0
 
-stop = -1
+stop = 0
 
 
 route_number = int(input("What is the route number: "))
+print(f"Bus Route Number = {route_number}")
 start = int(input("How many passengers waiting for bus at start of route? :"))
+print(f"Bus passengers at start of route = {start}")
 if start > max_passengers:
-    print("Sorry, our maximum capacity is 35, some will need to wait for next bus")
-    unhappy += start - max_passengers
+    print("Sorry, our maximum capacity is 35, some will need to wait for the  next bus")
+    unhappy = start - max_passengers
     current_load = 35
-    total_passanger += start
+    total_passengers = start
+
 else:
     current_load = start
-    total_passanger += start
+    total_passengers = start
+    # print(f" = {current_load}")
 
 number_of_stops = int(input("Please enter the number of stops on this route: " ))
+print(f"Number of stops on this route = {number_of_stops}")
 
 
 
 
 while stop < number_of_stops:
-    get_on = int(input("How many waiting to get on at this stop?"))
-    get_off = int(input("How many to depart at this stop?"))
-    stop += 1
-    print(current_load)
-    avaialable_seats = max_passengers-current_load+get_off
-    print(avaialable_seats)
-    if get_on <= avaialable_seats:
-        current_load += get_on - get_off
+    get_on = int(input(f"How many waiting to get on at stop {stop + 1}?"))
+    print(f"Passengers waiting at bus stop {stop + 1} = {get_on}")
+    get_off = int(input(f"How many to depart at bus stop {stop + 1}?"))
+    print(f"Passengers waiting to depart bus at stop {stop + 1} = {get_off}")
+    # print(current_load)
+    available_seats = max_passengers - current_load - get_off + get_on
+ 
+    # print(f"Available capacity {available_seats}")
+    
+    
+
+    
+    
+
+
+    if get_on <= available_seats:
+        current_load = current_load + get_on - get_off
+        print(f"Current load = {current_load}")
+        total_passengers += get_on
+        print(f"Available capacity {max_passengers - current_load}")
+        stop += 1
+
+
     else:
-        left_on_stop= get_on-avaialable_seats
+        left_on_stop= get_on-available_seats
         unhappy += left_on_stop
         current_load += get_on-get_off-left_on_stop
-    print(unhappy)
-    print(current_load)
+        total_passengers += current_load
+        available_seats = 0
+        
+        print(f"else unhappy passengers = {left_on_stop}")
+        print(f"else Current load = {current_load}")
+        print(f"else Available capacity = {available_seats}")
+        stop += 1
 
-print(unhappy)
+
+
+
+
+print(f"Unhappy customers = {unhappy}")
+print(f"Total passengers at end of route = {total_passengers}")
+if unhappy == 0:
+    print("Customer satisfaction 100% guaranteed")
+else:
+    print(f"Ratio of happy to unhappy passengers{(total_passengers / unhappy) * 100}")
+print("Thankyou for travelling with us today!!")
+
         
 
 
@@ -62,30 +87,3 @@ print(unhappy)
     
 
 
-
-
-
-
-# n = int(input('How many stops does this route have?'))       #n is the number of items you want to enter
-# dict ={}                     
-# for i in range(n):        
-#     text = input().split()     #split the input text based on space & store in the list 'text'
-#     dict[text[0]] = text[1]       #assign the 1st item to key and 2nd item to value of the dictionary
-# print(dict)
-
-
-
-
-# x = how many stops will there be 
-# stop = (x)
-# dict{}
-# while i < x:
-     
-#     dict['Stop' [stop] ]"hot many passengers at stop  :" [stop] )
-#     stop += stop
-
-
-# bus=Bus(int(input('Bus number:')))
-
-
-# stops=int(input('Stops:'))
