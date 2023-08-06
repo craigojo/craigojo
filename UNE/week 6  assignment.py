b70ba26b8ca52ea73141d76299ebb55ae998acc2
craigojo <110014616+craigojo@users.noreply.github.com>
2023-08-06 17:39:27 +1000

    Completed

--- UNE/week 6  assignment.py
+++ UNE/week 6  assignment.py
@@ -1,11 +1,10 @@
 def factorial(n):
+  if n < 0:
+    return 1
   result = 1
-  n=1
-
-  while n != 0:
-    n = n - 1
-    result = result * n
-    return result
-
+  while n > 0:
+      result = result * n
+      n = n - 1
+  return result
 for i in range(-1, 5):
   print('Factorial of', i, 'is', factorial(i))
