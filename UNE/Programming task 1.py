fe03f51775e26ff7501a0c8ad26aa95cbd1da111
craigojo <110014616+craigojo@users.noreply.github.com>
2023-08-06 22:47:19 +1000

    updates

--- UNE/Programming task 1.py
+++ UNE/Programming task 1.py
@@ -1,13 +1,12 @@
 
 
 max_passengers = 35
-
 unhappy = 0
 total_passengers=0
-
 current_load = 0
-
 stop = 0
+available_seats = 0
+required = 0
 
 
 route_number = int(input("What is the route number: "))
@@ -15,10 +14,12 @@ print(f"Bus Route Number = {route_number}")
 start = int(input("How many passengers waiting for bus at start of route? :"))
 print(f"Bus passengers at start of route = {start}")
 if start > max_passengers:
-    print("Sorry, our maximum capacity is 35, some will need to wait for the  next bus")
+
     unhappy = start - max_passengers
     current_load = 35
     total_passengers = start
+    print(f"Sorry, our maximum capacity is 35, {unhappy} will need to wait for the  next bus")
+
 
 else:
     current_load = start
@@ -37,42 +38,53 @@ while stop < number_of_stops:
     get_off = int(input(f"How many to depart at bus stop {stop + 1}?"))
     print(f"Passengers waiting to depart bus at stop {stop + 1} = {get_off}")
     # print(current_load)
-    available_seats = max_passengers - current_load - get_off + get_on
+    
+    if get_on > get_off:
+        required = get_on - get_off
+        if current_load + required > max_passengers:
+            left_on_stop= max_passengers - current_load - required
+            if left_on_stop < 0:
+                    left_on_stop = left_on_stop * -1
+            unhappy += left_on_stop
+            current_load = max_passengers
+        else:
+
+            total_passengers += current_load + required
+        stop += 1
+        print(f"else unhappy passengers at stop {stop} = {left_on_stop}")
+        print(f"else Current load = {current_load}")
+        print(f"else Available capacity = {available_seats}")
+
+        
+    elif get_off > get_on:
+        required = get_off - get_on
+
+        current_load = current_load + required
+        print(f"Current load = {current_load}")
+        total_passengers += get_on
+        available_seats = max_passengers - current_load
+        print(f"Available capacity {available_seats}")
+        stop += 1
+
+    available_seats = max_passengers - current_load
+    print(available_seats, max_passengers, current_load, get_off, get_on, unhappy)
+
+ 
  
     # print(f"Available capacity {available_seats}")
     
     
 
     
-    
-
-
-    if get_on <= available_seats:
-        current_load = current_load + get_on - get_off
-        print(f"Current load = {current_load}")
-        total_passengers += get_on
-        print(f"Available capacity {max_passengers - current_load}")
-        stop += 1
-
-
-    else:
-        left_on_stop= get_on-available_seats
-        unhappy += left_on_stop
-        current_load += get_on-get_off-left_on_stop
-        total_passengers += current_load
-        available_seats = 0
-        
-        print(f"else unhappy passengers = {left_on_stop}")
-        print(f"else Current load = {current_load}")
-        print(f"else Available capacity = {available_seats}")
-        stop += 1
+ 
 
 
 
 
 
-print(f"Unhappy customers = {unhappy}")
-print(f"Total passengers at end of route = {total_passengers}")
+
+print(f"Unhappy customers (Total) = {unhappy}")
+print(f"Total of all passengers for route = {total_passengers}")
 if unhappy == 0:
     print("Customer satisfaction 100% guaranteed")
 else:
