93db5d7ec6409a28665459b77bc46dd1ec3b7bc4
craigojo <110014616+craigojo@users.noreply.github.com>
2023-08-13 00:45:30 +1000

    Update randomizer removed While True to save lines

--- UNE/COSC01/Weekly Assignments/randomizer.py
+++ UNE/COSC01/Weekly Assignments/randomizer.py
@@ -1,19 +1,19 @@
 
 import random
 
-i = random.randit(1, 99)
+i = random.randint(1, 5)
 
 guess = int(input("Enter an integer from 1 to 99: "))
 
-while True:
+while guess != i:
         if guess < i:
                 print("Guess is low")
                 guess = int(input("Enter an integer form 1 to 99: "))
         elif guess > i:
                 print("Guess is high")
-                guess(input("Enter an integer from 1 to 99: "))
-        else:
-                print("You guessed it right!! Goodbye :)")
-                break
+                guess = int(input("Enter an integer from 1 to 99: "))
+
+print("You guessed it right!! Goodbye :)")
+
 
 
