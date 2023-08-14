0fa81feca1d15d26bb1111d493180fe27daf34ca
craigojo <tippers-basalts.0z@icloud.com>
2023-08-14 21:33:54 +1000

    Kooky calculator

---
+++ UNE/COSC01/Weekly Assignments/Tutorial 6/calculator.py
@@ -0,0 +1,61 @@
+
+def add(x, y):
+    add = x + y
+    return add
+
+def sub(x, y):
+    sub = x - y
+    return sub
+
+
+def mul(x, y):
+    mul = x * y
+    return mul
+
+def div(x, y):
+    div = x / y
+    return div
+    
+while True:
+    try:
+        num_1 = int(input("Enter value for x " ))            
+    except ValueError:
+        print("Try again")
+    else:
+        break
+        
+        
+
+
+while True:
+    try:
+        num_2 = int(input("Enter valu efor y "))
+    except ValueError:
+        print("Try again")
+    else:
+        break
+
+if num_2 > num_1:
+    val_a = mul(num_1, num_2)
+    val_b = add(num_1, num_2)
+    calculate_strange_value = val_a * val_b
+    print(calculate_strange_value)
+
+elif num_1 > num_2:
+    val_a = div(num_1, num_2)
+    val_b = sub(num_1, num_2)
+    calculate_strange_value = int(val_a * val_b)
+    print(calculate_strange_value)
+
+elif num_1 == num_2:
+    print("1")
+
+
+
+
+
+
+
+
+
+
