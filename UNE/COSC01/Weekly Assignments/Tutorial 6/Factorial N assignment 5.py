3fd999192527b249582ab851918b5251adb79858
craigojo <tippers-basalts.0z@icloud.com>
2023-08-14 21:33:34 +1000

    Updates with assertions

--- UNE/COSC01/Weekly Assignments/Tutorial 6/Factorial N assignment 5.py
+++ UNE/COSC01/Weekly Assignments/Tutorial 6/Factorial N assignment 5.py
@@ -1,9 +1,10 @@
 def factorial(n):
-
+  assert i > -1
     
 
   result = 1
-  while n != 0:
+  while n > 0:
+    assert n > 0
     result = result * n
     n = n - 1
     
@@ -12,6 +13,8 @@ def factorial(n):
 
 
 for i in range(-1, 5):
+  assert i > 0
+
   if i < 1:
     pass
   else:
