624cdb9446dc548d83ed3c127dd16f7a0e3a48c8
craigojo <tippers-basalts.0z@icloud.com>
2023-08-15 23:22:45 +1000

    rename file

---
+++ UNE/UDEMY/practice_file.py
@@ -0,0 +1,15 @@
+# a = "craig"
+# print(len(a))
+
+
+# print("Welcome")
+
+# city_name = input("Whats the name of the city you grew up in?\n ")
+# print(city_name)
+
+# pet_name  = input("Name of your pet?\n ")
+# print(pet_name)
+
+# print(city_name + ' ' + pet_name)
+
+print("Hello"[0,3,1])
