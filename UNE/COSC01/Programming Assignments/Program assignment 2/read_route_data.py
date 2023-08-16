51fca31e4a865c9a0847ec1affd1d853d5e998b9
craigojo <tippers-basalts.0z@icloud.com>
2023-08-16 21:30:14 +1000

    Create new file for assignment

---
+++ UNE/COSC01/Programming Assignments/Program assignment 2/read_route_data.py
@@ -0,0 +1,42 @@
+file_name = "routes.txt"
+
+# data = read_route_data[]
+
+
+
+
+
+def read_route_data():
+    new_route_keys['route_number', 'n_unhappy', 'n_happy']
+    new_route_data[]
+
+    try:
+
+        with open(file_name, "r") as file:
+
+            read_route_data = file.read()
+            # file_name.close()
+            print(read_route_data[0])
+            print(read_route_data[1])
+            print(read_route_data[2])
+            print(read_route_data[3])
+            print(read_route_data[4])
+            print(read_route_data[5])
+            print(read_route_data[6])
+            print(read_route_data[7])
+            print(read_route_data[8])
+            print(read_route_data[9])
+            print(read_route_data[10])
+            print(read_route_data[3])
+            
+    except:
+        FileNotFoundError
+        print("File not found, please check path")
+
+    
+        
+
+
+read_route_data()
+
+# def sort_route_data:
