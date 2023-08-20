f4a1e8a5f3b53bb889f64e19cc12094e9291dd46
craigojo <tippers-basalts.0z@icloud.com>
2023-08-20 13:53:45 +1000

    Updates to formatting

---
+++ UNE/COSC01/Weekly Assignments/Tutorial 7/quick sort.py
@@ -0,0 +1,17 @@
+def pivot(l, a, b):
+    p = l[(a + b)//2]
+    i, j = a - 1, b + 1
+    while True:
+        i, j = i + 1, j - 1
+        while l[i] < p: i = i + 1
+        while l[j] > p: j = j - 1
+        if i >= j: return j
+        l[i], l[j] = l[j], l[i]
+def quicksort(l, a, b):
+    if a < b:
+        p = pivot(l, a, b)
+        quicksort(l, a, p)
+        quicksort(l, p + 1, b)
+    return l
+def qs(l):
+    return quicksort(l, 0, len(l) - 1)
