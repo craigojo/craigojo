bb1298e50a16d343e5bf5e554e1a8e48ec57296b
craigojo <tippers-basalts.0z@icloud.com>
2023-08-25 19:52:17 +1000

    Add comments and test

--- UNE/COSC01/Programming Assignments/Program assignment 2/main.py
+++ UNE/COSC01/Programming Assignments/Program assignment 2/main.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
+'''Main program file'''
+
+
 # Functions required for the program are imported
 from operator import itemgetter
-
 
 
 # Resources imported from data_file_read file.
@@ -9,14 +11,13 @@ from data import read_route_data
 from data import data_list
 
 
-
 # Sets variables
 extra_buses = 0
 
 
-
-# Retrieves sorted data using the the "happy_ratio" from the read_dat_file function, sorts in ascending order
-# using the itemgetter function. Unexpected errors are handled here. Data purity is ensured within the read_data_function.
+# Retrieves data_list from the read_dat_file function and sorts using the the "happy_ratio". Sorts in ascending order
+# using the itemgetter function. Unexpected (unlikely) errors are handled here. 
+# Data purity is ensured within the read_data_function in file (routes.py).
 def sort_route_data(): 
     try:
         sorted_list = sorted(data_list, key=itemgetter("happy_ratio"), reverse = True)
@@ -25,32 +26,42 @@ def sort_route_data():
         print("An un-expected error occured")
 
 
-
 # Inputer is asked to enter number of extra busses that are available. 
-# Assumes maximum number of busses wont exceed number of routes.
+# Maximum number of busses cannot exceed number of routes.
 # Error are handled. Inputer is asked to enter a valid integer.
 def extra_bus_routes():
     while True:
         try:
-            extra_buses = int(input("How many routes can have an extra bus? "))
+
+# Inputer is prmopted to enter how many routes can have an extra bus (n)
+            n = int(input("How many routes can have an extra bus? "))
             list_length = len(sorted_data_list)
-            
-            if (extra_buses > 0) and extra_buses <= len(sorted_data_list):   
+
+# Input values are validated. n cannot be greater than the number of routes.            
+            if (n > 0) and n <= len(sorted_data_list):
+
+# Extra buses are allocated to routes, allocation of buses to routes is based on happy_ratio, least happy to happy.  
                 print('You should add busses for the following routes:')
-                for bus in range(extra_buses):
+                for bus in range(n):
                         extra_bus_route = sorted_data_list[bus]
                         print(f"    " + f'{extra_bus_route["route"]}')
                 break
-            elif extra_buses <= 0:
+
+# Values less than or equal to zero are rejected
+            elif n <= 0:
                  print("Enter an integer with value greater than 0")
+
+# Values greater than number of routes are rejected. 
+# User is prompted to enter a value equal to or less than number of routes
             else:
-                 print(f"The value you entered {extra_buses}," + "\n" + "is greater than the number of available routes." + "\n" + f"Enter a value no greater than {list_length}" + "\n" + f"{extra_buses - list_length} busses are free for other routes" + "\n")
-                
+                 print(f"The value you entered {n}," + "\n" + "is greater than the number of available routes." + "\n" + f"Enter a value no greater than {list_length}" + "\n" + f"{n - (len(sorted_data_list))} busses are free for other routes" + "\n")
+                 print(len(sorted_data_list))
+                 
+# Non integer values are handles. User is prompted to enter an integer  
         except (ValueError):
                 print("Please enter a value greater than 0, no letters or characters.")
 
-
-# Data read function is called, sorted data is stored in a variable.
+# Data read function is called, sorted data is stored in a variable (data_list()).
 # The extra bus function is then called
 read_route_data()
 sorted_data_list = sort_route_data()
