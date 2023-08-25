5f4a7e9d96e02e7e02f43e3a8dedd6fb24213d11
craigojo <tippers-basalts.0z@icloud.com>
2023-08-25 19:53:15 +1000

    new file

---
+++ UNE/COSC01/Weekly Assignments/Week 3/Tutorials/age.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+age_in_years = input('Tell me your age in years: ')
+age_in_years_int = int(age_in_years)
+age_in_dayss = (age_in_years_int ) * 365
+age_in_days = int(age_in_years) * 365
+
+print ("You are ", age_in_dayss, " days old!")
+print (age_in_days)
