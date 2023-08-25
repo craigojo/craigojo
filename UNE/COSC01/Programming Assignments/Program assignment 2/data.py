26f4e8d2f04f8a014e089351cf4238262105d71c
craigojo <tippers-basalts.0z@icloud.com>
2023-08-25 20:16:43 +1000

    Add comments

--- UNE/COSC01/Programming Assignments/Program assignment 2/data.py
+++ UNE/COSC01/Programming Assignments/Program assignment 2/data.py
@@ -1,49 +1,65 @@
-# Functions required for the program are imported
+# Functions required for the program are imported (regex)
 import re
 
-# File containing bus data is refrenced here. The storage file is created.
+# File containing raw route data is refrenced here. Is allocated a variable.
 file_name = "routes.txt"
+
+# Stores filtered data retrieved from the raw data file, that has passed the read_route_data function.
 data_list = []
 
 
 def read_route_data():
-    data_list.clear()               # Clears the storage file from previous entries
+# Clears the storage file from previous entries.
+    data_list.clear()               
     with open(file_name, "r") as file:
         lines = file.readlines()
-        for line_number, line in enumerate(lines, start=1):                 # Enumerate loops through the text file
-            values = line.strip().split(',')                                # File is read line by line, Spaces are removed, contents indentified by commas.
-            if not re.match(r'^\d+(?:,\d+){2}$', line):                     # regex is used to capture 3 groups of integers per line
+        for line_number, line in enumerate(lines, start=1):
+# Iterates through the text file
+# Spaces are removed, contents indentified by commas.            
+            values = line.strip().split(',')
 
-                
-                print(f"Error in line {line_number}: Incorrect format")     # Integers only, in groupings of 3 
+# File is read line by line,
+# regex is used to capture 3 groups of integers per line                            
+            if not re.match(r'^\d+(?:,\d+){2}$', line):
+
+ # Integers only, in groupings of 3                
+                print(f"Error in line {line_number}: Incorrect format")     
                 break
-
             try:
                 route = int(values[0])
-            except ValueError:                                                              # Captures invalid entries, accepts integers only.
+
+# Captures invalid entries, accepts integers only.
+            except ValueError:                                                              
                 print("Input file must only contain numbers separated by comma's.")     
                 break
 
-            try:                                                                            
-                if route not in [routes["route"] for routes in data_list]:                  # Eliminates potential for DivideByZero error,
+
+            try:
+# Eliminates potential for DivideByZero error.                                                                            
+                if route not in [routes["route"] for routes in data_list]:                  
                     n_happy = int(values[1])                                                
                     n_unhappy = int(values[2])
                     if n_unhappy < 1:
                         happy_ratio = 0
-                    else:
-                                                                                            
-                        happy_ratio = float(format(n_happy/n_unhappy, ".2f"))               # Calculates happy_ratio
-                    
-                    dictionary = {                                                          # Stores values into a dictionary
+
+# Calculates happy_ratio 
+                    else:                                                                                           
+                        happy_ratio = float(format(n_happy/n_unhappy, ".2f"))
+
+# Associates key with a value.                   
+                    dictionary = {                                                          
                         "route": int(values[0]),
                         "n_happy": int(values[1]),
                         "n_unhappy": int(values[2]),
                         "happy_ratio": happy_ratio
                     }
-                    data_list.append(dictionary)                                            # Dictionary is added to the data_list store.
+# Stores values into a new dictionary within list, data_list.
+                    data_list.append(dictionary)
+# Captures duplicates within data. Prompts user to check file. Program exits                                          
                 else:
                     print(f"Duplicate route number in line number {line_number} of the {file_name} input file." + "\n" + f"Please verify {file_name} file contains no duplicated data and try again")
                     break
+# Captures exceptions. IndexError. Prompts user to verify raw file contains integers in groups of 3 per line, separated by commas only, no spaces.
             except IndexError:
                 print("Please confirm each text file row contains values separated by comma's, no spaces, for the following" + "\n" + "- Route Number" +"\n"+ "- Number Happy" +"\n" + "- Number Unhappy" )
 
