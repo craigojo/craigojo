271eed10c35a9cec66490c93f305f1c9c148125d
craigojo <tippers-basalts.0z@icloud.com>
2023-09-10 21:16:28 +1000

    Add code this brings error to front

--- UNE/COSC01/Programming Assignments/Program Assignment 3/bus_gui2.py
+++ UNE/COSC01/Programming Assignments/Program Assignment 3/bus_gui2.py
@@ -4,19 +4,19 @@ from tkinter import *
 def create_widget(label2):
     try:
         my_label1 = Label(root, text="Rate the cleanliness of the bus:")
-        my_label2 = Label(root, text="Please Select a Rating")
+        rating_selected = Label(root, text="Please Select a Rating")
     
 
         my_button1 = Button(root, text=":-(", padx=25, borderwidth=4, state=DISABLED)
-        my_button2 = Button(root, text="1", padx=25, borderwidth=4, command=lambda: rating(1, my_label2))
-        my_button3 = Button(root, text="2", padx=25, borderwidth=4, command=lambda: rating(2, my_label2))
-        my_button4 = Button(root, text="3", padx=25, borderwidth=4, command=lambda: rating(3, my_label2))
-        my_button5 = Button(root, text="4", padx=25, borderwidth=4, command=lambda: rating(4, my_label2))
-        my_button6 = Button(root, text="5", padx=25, borderwidth=4, command=lambda: rating(5, my_label2))
+        my_button2 = Button(root, text="1", padx=25, borderwidth=4, command=lambda: rating(1, rating_selected))
+        my_button3 = Button(root, text="2", padx=25, borderwidth=4, command=lambda: rating(2, rating_selected))
+        my_button4 = Button(root, text="3", padx=25, borderwidth=4, command=lambda: rating(3, rating_selected))
+        my_button5 = Button(root, text="4", padx=25, borderwidth=4, command=lambda: rating(4, rating_selected))
+        my_button6 = Button(root, text="5", padx=25, borderwidth=4, command=lambda: rating(5, rating_selected))
         my_button7 = Button(root, text=":-)", padx=25, borderwidth=4, state=DISABLED)
 
         my_label1.grid(row=0, column=3, columnspan=3)
-        my_label2.grid(row=3, column=3, columnspan=3)
+        rating_selected.grid(row=3, column=3, columnspan=4)
 
         my_button1.grid(row=2, column=1)
         my_button2.grid(row=2, column=2)
@@ -54,12 +54,13 @@ try:
     root.resizable(False, False)
     total_rating = 0
     ratings = 0
-    my_label2 = Label(root, text="")
-    create_widget(my_label2)
+    rating_selected = Label(root, text="")
+    create_widget(rating_selected)
 
 
 except TclError as widget_init_error:
     root = Tk()
+    root.attributes("-topmost", True)
     root.title("Initialisation Error")
     root.geometry("150x100")
     root.minsize(150, 100)
