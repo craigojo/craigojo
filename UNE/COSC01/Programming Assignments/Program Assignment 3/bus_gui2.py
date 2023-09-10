6aad29e54669c976b76e00a72e1e11ee0421b403
craigojo <tippers-basalts.0z@icloud.com>
2023-09-10 22:59:46 +1000

    Updates for font size

--- UNE/COSC01/Programming Assignments/Program Assignment 3/bus_gui2.py
+++ UNE/COSC01/Programming Assignments/Program Assignment 3/bus_gui2.py
@@ -3,28 +3,28 @@ from tkinter import *
 
 def create_widget(label2):
     try:
-        my_label1 = Label(root, text="Rate the cleanliness of the bus:")
-        rating_selected = Label(root, text="Please Select a Rating")
+        my_label1 = Label(root, anchor="center",font="10", text="Rate the cleanliness of the bus:")
+        rating_selected = Label(root, anchor="center",font="10", text="")
     
 
-        my_button1 = Button(root, text=":-(", padx=25, borderwidth=4, state=DISABLED)
-        my_button2 = Button(root, text="1", padx=25, borderwidth=4, command=lambda: rating(1, rating_selected))
-        my_button3 = Button(root, text="2", padx=25, borderwidth=4, command=lambda: rating(2, rating_selected))
-        my_button4 = Button(root, text="3", padx=25, borderwidth=4, command=lambda: rating(3, rating_selected))
-        my_button5 = Button(root, text="4", padx=25, borderwidth=4, command=lambda: rating(4, rating_selected))
-        my_button6 = Button(root, text="5", padx=25, borderwidth=4, command=lambda: rating(5, rating_selected))
-        my_button7 = Button(root, text=":-)", padx=25, borderwidth=4, state=DISABLED)
+        my_button1 = Button(root, text=":-(", padx=10, borderwidth=4, state=DISABLED)
+        my_button2 = Button(root, text="1", padx=50, borderwidth=4, command=lambda: rating(1, rating_selected))
+        my_button3 = Button(root, text="2", padx=50, borderwidth=4, command=lambda: rating(2, rating_selected))
+        my_button4 = Button(root, text="3", padx=50, borderwidth=4, command=lambda: rating(3, rating_selected))
+        my_button5 = Button(root, text="4", padx=50, borderwidth=4, command=lambda: rating(4, rating_selected))
+        my_button6 = Button(root, text="5", padx=50, borderwidth=4, command=lambda: rating(5, rating_selected))
+        my_button7 = Button(root, text=":-)", padx=10, borderwidth=4, state=DISABLED)
 
-        my_label1.grid(row=0, column=3, columnspan=3)
-        rating_selected.grid(row=3, column=3, columnspan=4)
+        my_label1.grid(row=0, column=2, columnspan=6)
+        rating_selected.grid(row=3, column=2, columnspan=6)
 
-        my_button1.grid(row=2, column=1)
-        my_button2.grid(row=2, column=2)
-        my_button3.grid(row=2, column=3)
-        my_button4.grid(row=2, column=4)
-        my_button5.grid(row=2, column=5)
-        my_button6.grid(row=2, column=6)
-        my_button7.grid(row=2, column=7)
+        my_button1.grid(row=2, column=1, padx=(33, 0), pady=(5, 5))
+        my_button2.grid(row=2, column=2, padx=(0, 0), pady=(5, 5))
+        my_button3.grid(row=2, column=3, padx=(0, 0), pady=(5, 5))
+        my_button4.grid(row=2, column=4, padx=(0, 0), pady=(5, 5))
+        my_button5.grid(row=2, column=5, padx=(0, 0), pady=(5, 5))
+        my_button6.grid(row=2, column=6, padx=(0, 0), pady=(5, 5))
+        my_button7.grid(row=2, column=7, padx=(0, 10), pady=(5, 5))
 
     except TclError as widget_tcl_error:
         
@@ -48,9 +48,9 @@ def rating(value, return_label):
 try:     
     root = Tk()
     root.title("Cleanliness Rater")
-    root.geometry("500x100")
-    root.minsize(500, 100)
-    root.maxsize(500, 100)
+    root.geometry("750x95")
+    root.minsize(750, 95)
+    root.maxsize(750, 95)
     root.resizable(False, False)
     total_rating = 0
     ratings = 0
