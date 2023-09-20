eba20da11ac74bdab1024d306aeb927746a86f31
craigojo <tippers-basalts.0z@icloud.com>
2023-09-20 14:03:34 +1000

    updates

--- UNE/COSC01/Weekly Assignments/Week 3/years_to_onehundred.py
+++ UNE/COSC01/Weekly Assignments/Week 3/years_to_onehundred.py
@@ -7,3 +7,17 @@ elif age_in_years < 0:
 else:
     years_to_onehundred = 100 - age_in_years
     print("You will be 100 in",(years_to_onehundred), "years")
+
+for age in age_in_years:
+    try:
+        age > 0 and age < 100
+        calc_age = int(100 - age_in_years)
+        print("You will be 100 in", calc_age, "Years")
+
+        
+    except:
+        ValueError
+        print("Try again")
+    else:
+
+        print("Enter a valid number between 0 and 100")
