3b037c4fa056384e4e579537af5a90f10cd9a5f7
craigojo <tippers-basalts.0z@icloud.com>
2023-09-20 15:18:32 +1000

    updated version with Y/N loop

---
+++ UNE/COSC01/Weekly Assignments/Week 4/draw_grid_enhanced.py
@@ -0,0 +1,39 @@
+
+
+def draw_grid(width, height):
+    end_line = '+'
+    internal_line = '|'
+
+    for i in range(width):
+        end_line = end_line + '-'
+        internal_line = internal_line + ' '
+    end_line = end_line + '+'
+    internal_line = internal_line + '|'
+    print(end_line)
+
+    for i in range(height):
+        print(internal_line)
+
+    print(end_line)
+
+while True:
+        a = int(input("enter a value for width"))
+        b = int(input("enter a value for height"))
+
+        draw_grid(a, b)
+
+        again = input(str("Play again?"))
+
+        if again.upper() != 'Y':
+            break
+
+
+
+
+
+
+
+
+
+    
+
