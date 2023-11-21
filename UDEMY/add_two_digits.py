9083623379073dbd9e63bab88af0e40a4d997cb4
Craig Dennes <110014616+craigojo@users.noreply.github.com>
2023-11-22 07:58:55 +1100

    update

--- UDEMY/add_two_digits.py
+++ UDEMY/add_two_digits.py
@@ -6,3 +6,12 @@ print(int(two_digit_number[0]) + int(two_digit_number[1]))
 two_digit_number = input()
 result = int(two_digit_number[0]) + int(two_digit_number[1])
 print(result)
+
+
+
+two_digit_number = input()
+first_digit = int(two_digit_number[0])
+second_digit = int(two_digit_number[1])
+
+two_digit_number = first_digit + second_digit
+print(two_digit_number)
