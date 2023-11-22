5b6d804e338600fc5d2c3c2225b4e62162a8a8e5
Craig Dennes <110014616+craigojo@users.noreply.github.com>
2023-11-23 07:57:00 +1100

    Update to include photo day 3 lesson 35

--- UDEMY/Rollercoaster.py
+++ UDEMY/Rollercoaster.py
@@ -1,14 +1,45 @@
+# print("Welcome to the Rollercoaster")
+# height = int(input("What is your height in cm? "))
+
+# cost = 0
+# if height >= 120:
+#     print("You can ride the rollercoaster!")
+#     age = int(input("What is your age? "))
+#     if age < 12:
+#         cost += 5
+#     if age <=18:
+#         cost += 7
+#     else:
+#         cost += 12
+
+#     photo = input("Would you like a photo? Y/N")
+#     if photo == "Y":
+#         print(f"Please pay ${cost + 3}")
+#     else:
+#         print(f"Please pay ${cost}")
+# else:
+#     (print("Sorry, you have to grow taller before you can ride"))
+
+
+
+
 print("Welcome to the Rollercoaster")
 height = int(input("What is your height in cm? "))
 
+cost = 0
 if height >= 120:
-    print("You can rid ethe rollercoaster!")
+    print("You can ride the rollercoaster!")
     age = int(input("What is your age? "))
     if age < 12:
-        print("Please pay $5")
+        cost += 5
     if age <=18:
-        print("Please pay $7")
+        cost += 7
     else:
-        print("Please pay $12")
+        cost += 12
+
+    photo = input("Would you like a photo? Y/N")
+    if photo == "Y":
+        cost += 3
+    print(f"Please pay ${cost}")
 else:
     (print("Sorry, you have to grow taller before you can ride"))
