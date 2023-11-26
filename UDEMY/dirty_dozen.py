58cfa8a4e2fd2347f1a39fab57cb1d8169fddea2
Craig Dennes <110014616+craigojo@users.noreply.github.com>
2023-11-27 07:35:37 +1100

    day 4 lesson 44

---
+++ UDEMY/dirty_dozen.py
@@ -0,0 +1,12 @@
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+     
+dirty_dozen = [fruits, vegetables]
+     
+print(dirty_dozen[1][1])
+
+print(dirty_dozen[0])
+print(dirty_dozen[1])
+
+print(dirty_dozen[1][2])
+print(dirty_dozen[1][3])
