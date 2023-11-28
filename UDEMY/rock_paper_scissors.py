bdfbe427124c98d291944bae511831695dc2e372
Craig Dennes <110014616+craigojo@users.noreply.github.com>
2023-11-29 08:23:42 +1100

    updates

--- UDEMY/rock_paper_scissors.py
+++ UDEMY/rock_paper_scissors.py
@@ -37,48 +37,91 @@ rock_paper_scissors = str(input("Choose: rock, paper or scissors?")).lower()
 
 if random_integer == 0:
     challenge = "rock"
-    rock_rand = 1
+    rand = 1
 elif random_integer == 1:
     challenge = "paper"
-    paper_rand = 2
+    rand = 2
 elif random_integer == 2:
     challenge = "scissors"
-    scissors_rand = 3
+    rand = 3
 
 if rock_paper_scissors == "rock":
-   rock_challenge = 1
+   input_challenge = 1
 elif rock_paper_scissors == "paper":
-   paper_challenge = 2
+   input_challenge = 2
 elif rock_paper_scissors == "scissors":
-   paper_challenge = 3
+   input_challenge = 3
 
 
 
 if challenge == rock_paper_scissors:
     print("Draw!")
-    heads_tails = "heads"
-elif:
-    heads_tails = "tails"
+
+elif  rand < input_challenge < 3 :
+    print("You Win!")
 if challenge == heads_tails:
     print("You Win!")
 else:
     print("You Loose!")
 
 
-choice1 = input('Choose: Rock, paper or scissors?" \n').lower()
-if choice1 == "rock":
-  choice2 = input('You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across. \n').lower()
-  if choice2 == "wait":
-    choice3 = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose? \n").lower()
-    if choice3 == "red":
-      print("It's a room full of fire. Game Over.")
-    elif choice3 == "yellow":
-      print("You found the treasure! You Win!")
-    elif choice3 == "blue":
-      print("You enter a room of beasts. Game Over.")
-    else:
-      print("You chose a door that doesn't exist. Game Over.")
-  else:
-    print("You get attacked by an angry trout. Game Over.")
-else:
-  print("You fell into a hole. Game Over.")
+
+import random
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+game_images = [rock, paper, scissors]
+
+user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+print(game_images[user_choice])
+
+computer_choice = random.randint(0, 2)
+print("Computer chose:")
+print(game_images[computer_choice])
+
+if user_choice >= 3 or user_choice < 0: 
+  print("You typed an invalid number, you lose!") 
+elif user_choice == 0 and computer_choice == 2:
+  print("You win!")
+elif computer_choice == 0 and user_choice == 2:
+  print("You lose")
+elif computer_choice > user_choice:
+  print("You lose")
+elif user_choice > computer_choice:
+  print("You win!")
+elif computer_choice == user_choice:
+  print("It's a draw")
+
+####### Debugging challenge: #########
+#Try running this code and type 5.
+#It will give you an IndexError and point to line 32 as the issue.
+#But on line 38 we are trying to prevent a crash by detecting
+#any numbers great than or equal to 3 or less than 0.
+#So what's going on?
+#Can you debug the code and fix it?
+#Solution: https://repl.it/@appbrewery/rock-paper-scissors-debugged-end
